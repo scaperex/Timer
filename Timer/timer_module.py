42d40d1e08b74cb813eac5cf20d91c413f245ab5
Omer S <omer.shubi@gmail.com>
2020-04-26 23:46:36 +0300

    Update timer_module.py
    change log level from info to debug

--- Timer/timer_module.py
+++ Timer/timer_module.py
@@ -19,7 +19,7 @@ class Timer:
 
     def start(self):
         self.__start_time = time()
-        self.logger.info("Started {}".format(self.name))
+        self.logger.DEBUG("Started {}".format(self.name))
 
     def stop(self):
         self.__end_time = time()
@@ -34,22 +34,22 @@ class Timer:
             hours = elapsed / 3600
             minutes = hours % 60
             hours = floor(hours)
-            self.logger.info("{} took {} hours and {:.2f} {} to complete".format(self.name, hours, minutes, unit))
+            self.logger.DEBUG("{} took {} hours and {:.2f} {} to complete".format(self.name, hours, minutes, unit))
         elif elapsed >= 60:
             minutes = floor(elapsed / 60)
             seconds = elapsed % 60
-            self.logger.info("{} took {} minutes and {:.2f} {} to complete".format(self.name, minutes, seconds, unit))
+            self.logger.DEBUG("{} took {} minutes and {:.2f} {} to complete".format(self.name, minutes, seconds, unit))
         elif elapsed < 0.1:
             unit = "ms"
-            self.logger.info("{} took {:.2f} {} to complete".format(self.name, elapsed * 1000, unit))
+            self.logger.DEBUG("{} took {:.2f} {} to complete".format(self.name, elapsed * 1000, unit))
         else:
-            self.logger.info("{} took {:.2f} {} to complete".format(self.name, elapsed, unit))
+            self.logger.DEBUG("{} took {:.2f} {} to complete".format(self.name, elapsed, unit))
 
     def __init_logger(self, logger):
         if logger:
             return logger
         logger = logging.getLogger(__name__)
-        logger.setLevel(logging.INFO)
+        logger.setLevel(logging.DEBUG)
         if not logger.hasHandlers():
             formatter = logging.Formatter('{asctime} - {message}', datefmt="%H:%M:%S", style="{")
             handler = logging.StreamHandler()
